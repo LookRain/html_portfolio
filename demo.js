0e68e844e03ea9df295b707b1273cf2a22849ce1
LookRain <brantscube@hotmail.com>
2017-02-02 17:45:24 +0800

    changed style, added jQuery particle effect

---
+++ demo.js
@@ -0,0 +1,27 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#65a3d8',
+    lineColor: '#65a3d8'
+  });
+  var intro = document.getElementById('wrapper');
+  intro.style.marginTop = - intro.offsetHeight  + 'px';
+}, false);
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
